e7f4f538a85466989f6cc4cc866b1e13ddb95c43
thesonofdq7701 <109153555+thesonofdq7701@users.noreply.github.com>
2024-04-06 12:51:22 +0700

    Update GUI_sentiment_analysis.py

--- GUI_sentiment_analysis.py
+++ GUI_sentiment_analysis.py
@@ -132,7 +132,7 @@ elif choice=='Predict New Data':
             df['Processed_Comment'] = optimized_process_text(df['Comment'])
             predictions = model_SVM.predict(df['Processed_Comment'])
             st.write(df)
-    Từ df này, người dùng có thể thực hiện các xử lý dữ liệu khác nhau
+    # Từ df này, người dùng có thể thực hiện các xử lý dữ liệu khác nhau
     submitted_project1 = st.button("Dự đoán cảm xúc")
     if submitted_project1:
         st.write("Hiển thị kết quả dự đoán cảm xúc...")
